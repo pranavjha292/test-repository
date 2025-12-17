9e558605712374810b9d6383385de7ebc6bd1a9f
Pranav Jha <pjpranavjha66@outlook.com>
2025-12-17 18:45:41 +0530

    test-repository

--- src/main/java/com/techie/springbootrediscache/service/ProductService.java
+++ src/main/java/com/techie/springbootrediscache/service/ProductService.java
@@ -3,27 +3,46 @@ package com.techie.springbootrediscache.service;
 import com.techie.springbootrediscache.dto.ProductDto;
 import com.techie.springbootrediscache.entity.Product;
 import com.techie.springbootrediscache.repository.ProductRepository;
+import lombok.extern.slf4j.Slf4j;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.cache.Cache;
+import org.springframework.cache.CacheManager;
+import org.springframework.cache.annotation.CacheEvict;
+import org.springframework.cache.annotation.CachePut;
+import org.springframework.cache.annotation.Cacheable;
 import org.springframework.stereotype.Service;
-
 @Service
+@Cacheable
 public class ProductService {
 
-    private final ProductRepository productRepository;
+    private static final Logger log = LoggerFactory.getLogger(ProductService.class);
 
-    public ProductService(ProductRepository productRepository) {
+    private final ProductRepository productRepository;
+    private final CacheManager cacheManager;
+
+    public ProductService(ProductRepository productRepository, CacheManager cacheManager) {
         this.productRepository = productRepository;
+        this.cacheManager = cacheManager;
     }
 
+    //@CachePut(value = "PRODUCT_CACHE", key = "#result.id()")
     public ProductDto createProduct(ProductDto productDto) {
+
+        log.info("createProduct");
         var product = new Product();
         product.setName(productDto.name());
         product.setPrice(productDto.price());
 
         Product savedProduct = productRepository.save(product);
+//        Cache productCache = cacheManager.getCache("PRODUCT_CACHE");
+//        assert productCache != null;
+//        productCache.put(product.getId(), savedProduct);
         return new ProductDto(savedProduct.getId(), savedProduct.getName(),
                 savedProduct.getPrice());
     }
 
+    //@Cacheable(value = "PRODUCT_CACHE", key ="#productId")
     public ProductDto getProduct(Long productId) {
         Product product = productRepository.findById(productId)
                 .orElseThrow(() -> new IllegalArgumentException("Cannot find product with id " + productId));
@@ -31,6 +50,7 @@ public class ProductService {
                 product.getPrice());
     }
 
+    //@CachePut(value = "PRODUCT_CACHE", key = "#result.id()")
     public ProductDto updateProduct(ProductDto productDto) {
         Long productId = productDto.id();
         Product product = productRepository.findById(productId)
@@ -44,6 +64,7 @@ public class ProductService {
                 updatedProduct.getPrice());
     }
 
+   // @CacheEvict(value = "PRODUCT_CACHE", key ="#productId")
     public void deleteProduct(Long productId) {
         productRepository.deleteById(productId);
     }
